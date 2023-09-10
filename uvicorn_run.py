dec407e6a549f12c433e5d563cc7127e0faeea20
Yoshikage Kira <trevorphillips@gmx.us>
2023-09-10 01:37:45 -0400

    Moved port number to .env file

--- uvicorn_run.py
+++ uvicorn_run.py
@@ -1,9 +1,13 @@
 #!venv/bin/python
 from multiprocessing import cpu_count
+from os import getenv
 
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, log_level="info", workers=cpu_count())
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=int(getenv("LEMMY_SEE_MY_HATERS_PORT", 8000)), log_level="info", workers=cpu_count())
     server = uvicorn.Server(config)
     server.run()
