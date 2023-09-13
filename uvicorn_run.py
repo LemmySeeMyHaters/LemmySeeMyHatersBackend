573b977a9eed3410decbd5f42a688d7908aaee69
Yoshikage Kira <trevorphillips@gmx.us>
2023-09-13 02:16:58 -0400

    Changed the host address to lookback address

--- uvicorn_run.py
+++ uvicorn_run.py
@@ -8,6 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=int(getenv("LEMMY_SEE_MY_HATERS_PORT", 8000)), log_level="info", workers=cpu_count())
+    config = uvicorn.Config("main:app", host="127.0.0.1", port=int(getenv("LEMMY_SEE_MY_HATERS_PORT", 8000)), log_level="info", workers=cpu_count())
     server = uvicorn.Server(config)
     server.run()
