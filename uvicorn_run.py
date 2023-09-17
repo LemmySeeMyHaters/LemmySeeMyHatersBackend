fc238f5df419e953c5495e7c7222aabb0de97028
Yoshikage Kira <trevorphillips@gmx.us>
2023-09-17 01:58:07 -0400

    Uvicorn now logs real ip

--- uvicorn_run.py
+++ uvicorn_run.py
@@ -8,6 +8,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    config = uvicorn.Config("main:app", host="127.0.0.1", port=int(getenv("LEMMY_SEE_MY_HATERS_PORT", 8000)), log_level="info", workers=cpu_count())
+    config = uvicorn.Config(
+        "main:app",
+        host="127.0.0.1",
+        port=int(getenv("LEMMY_SEE_MY_HATERS_PORT", 8000)),
+        log_level="info",
+        workers=cpu_count(),
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+    )
     server = uvicorn.Server(config)
     server.run()
